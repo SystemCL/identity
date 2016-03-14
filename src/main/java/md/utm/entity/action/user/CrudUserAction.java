bd8ebdfcf313bb35380701d7ea20abcfec59a07f
SystemCL <vladislav94@gmail.com>
2016-03-14 21:30:08 +0200

    Login User --> Test

---
+++ src/main/java/md/utm/entity/action/user/CrudUserAction.java
@@ -0,0 +1,90 @@
+package md.utm.entity.action.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+
+import md.utm.entity.model.dao.UserDAO;
+import md.utm.entity.model.entity.UserAc;
+
+public class CrudUserAction {
+
+	private UserAc user = new UserAc();
+
+	private UserDAO userDAO;
+
+	private List<UserAc> userList;
+
+	private Integer userId;
+
+	public List<UserAc> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<UserAc> userList) {
+		this.userList = userList;
+	}
+
+	public UserAc getModel() {
+		return user;
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String addUser() throws Exception {
+		userDAO.save(user);
+		if (user.getUsername() != null) {
+			return Action.SUCCESS;
+		}
+		return Action.ERROR;
+	}
+
+	public String updateUser() {
+		userDAO.saveOrUpdate(user);
+		return Action.SUCCESS;
+	}
+
+	@SuppressWarnings("unchecked")
+	public String editUser() {
+		String returnType = Action.NONE;
+		if (userId != null) {
+			ActionContext.getContext().getValueStack().getRoot().remove(user);
+			user = getUserDAO().findUser(userId);
+			ActionContext.getContext().getValueStack().getRoot().add(user);
+			returnType = Action.SUCCESS;
+		}
+		return returnType;
+	}
+
+	public String removeUser() {
+		if (userId != null) {
+			getUserDAO().deleteUser(userId);
+		}
+		return Action.SUCCESS;
+	}
+
+	// list all users
+	public String listAllUsers() {
+		userList = userDAO.getAllUsers();
+		if (userList == null) {
+			userList = new ArrayList<UserAc>();
+		}
+		return Action.SUCCESS;
+	}
+}
--- src/main/webapp/user/register.jsp
+++ src/main/webapp/user/register.jsp
@@ -2,12 +2,12 @@
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <s:form action="loginauth">
-	<s:textfield name="username" label="Name" value="%{username}" />
-	<s:textfield name="password" label="Surname" value="%{password}" />
-	<s:if test="%{idAccount==null}">
+	<s:textfield name="username" label="Username" value="%{username}" />
+	<s:textfield name="password" label="Password" value="%{password}" />
+	 <s:if test="%{idAccount==null}"> 
  	<s:submit value="Login" action="loginauth" />
-	</s:if>
+	 </s:if> 
 	<s:else>
-		<s:submit value="Update" action="registerUser" />
+		<s:submit value="Update" action="loginauth" />
 	</s:else>
 </s:form>
