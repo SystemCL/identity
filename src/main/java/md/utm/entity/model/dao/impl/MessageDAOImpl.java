7c617bec318c427cdd626618091cae6886d62d55
artiomsevciuc <artiomsevciuc@users.noreply.github.com>
2016-05-14 12:42:17 +0300

    Select for messages by profile id and sender id

--- src/main/java/md/utm/entity/model/dao/impl/MessageDAOImpl.java
+++ src/main/java/md/utm/entity/model/dao/impl/MessageDAOImpl.java
@@ -6,26 +6,35 @@ import java.util.Map;
 import com.opensymphony.xwork2.ActionContext;
 
 import md.utm.entity.model.dao.MessageDAO;
-import md.utm.entity.model.dao.UserDAO;
-import md.utm.entity.model.entity.Comment;
 import md.utm.entity.model.entity.Message;
 
 @SuppressWarnings("unchecked")
 public class MessageDAOImpl extends GenericDAOImpl implements MessageDAO {
 
 	public List<Message> getMessagesBySenderId() {
-		
+
 		Integer idProf;
-		Map session = ActionContext.getContext().getSession(); 
-		idProf = (Integer)session.get("profile_id");
-		
+		Map session = ActionContext.getContext().getSession();
+		idProf = (Integer) session.get("profile_id");
+
 		// trebuie de adus din sesiune id-ul la sender !!!!!!!!!!!!!!!!!!!
-		return getHibernateTemplate().find("from Message");
-				
-				//getHibernateTemplate().find("from Message where idSender=1 AND idMessage in (Select idMessage from profile_messages where profile_id=?)",idProf);
-		/*return getHibernateTemplate().find("from Message where idSender=1 AND idMessage in (Select idMessage from profile_messages where profile_id="+idProf+")");*/
-				//getHibernateTemplate().find("from Message where idSender=?" +idSender+ "AND idMessage in (Select idMessage from profile_messages where profile_id=1)");
-		/*?"+idProf+")"); */
+		// select us from User us where us not in (select u from User u join
+		// u.projects p where p.id=?)
+		return getHibernateTemplate()
+				.find("from Message m join m.profile_messages pm where pm.profile_id=? and m.idSender=?", idProf, 2);
+
+		// getHibernateTemplate().find("from Message where idSender=1 AND
+		// idMessage in (Select idMessage from profile_messages where
+		// profile_id=?)",idProf);
+		/*
+		 * return getHibernateTemplate().find(
+		 * "from Message where idSender=1 AND idMessage in (Select idMessage from profile_messages where profile_id="
+		 * +idProf+")");
+		 */
+		// getHibernateTemplate().find("from Message where idSender=?"
+		// +idSender+ "AND idMessage in (Select idMessage from profile_messages
+		// where profile_id=1)");
+		/* ?"+idProf+")"); */
 	}
 
 	public Message getMessageById(Integer idMessage) {
@@ -34,18 +43,20 @@ public class MessageDAOImpl extends GenericDAOImpl implements MessageDAO {
 	}
 
 	public List<Message> getMessagesForConversation(Integer idProfile) {
-		
+
 		Integer idProf;
-		Map session = ActionContext.getContext().getSession(); 
-		idProf = (Integer)session.get("profile_id");
-		
-		//select * from Message where idSender=? and where idMessage=
-		//         ( message_id from profile_message where profile_id = [profilul meu])
-		// aduc doate mesajele de la senderul dat unde se contine in tabela intermediara 
+		Map session = ActionContext.getContext().getSession();
+		idProf = (Integer) session.get("profile_id");
+
+		// select * from Message where idSender=? and where idMessage=
+		// ( message_id from profile_message where profile_id = [profilul meu])
+		// aduc doate mesajele de la senderul dat unde se contine in tabela
+		// intermediara
 		// id- pentru meu - adica mesajele destinate mie
-		
-		//return getHibernateTemplate().find("from Message where idSender=?", idProfile);
+
+		// return getHibernateTemplate().find("from Message where idSender=?",
+		// idProfile);
 		return getHibernateTemplate().find("from Message ");
 	}
-	
+
 }
